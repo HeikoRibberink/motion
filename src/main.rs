049c2d3b4655cf227853b83d8b27bbf87024ec82
HeikoRibberink <heiko.ribberink@deblijdeaankomst.nl>
2022-02-05 11:58:01 +0100

    Motion tracking with mpu6050, first commit.

---
+++ src/main.rs
@@ -0,0 +1,60 @@
+use chrono::prelude::*;
+use embedded_hal::blocking::delay::DelayMs;
+use linux_embedded_hal::Delay;
+use mpu6050::Mpu6050;
+use rppal::i2c::I2c;
+use terminal;
+use tracker::Tracker;
+
+mod tracker;
+
+type MpuErr = mpu6050::Mpu6050Error<rppal::i2c::Error>;
+
+fn main() -> Result<(), MpuErr> {
+    //Setup i2c
+    let i2c = I2c::new().unwrap();
+
+    let mut mpu = Mpu6050::new(i2c);
+
+    //Accelerometer offsets of Mpu6050
+    mpu.acc_offsets.x = -0.022777887;
+    mpu.acc_offsets.y = -0.01322483;
+    mpu.acc_offsets.z = 0.04565797 - 0.0;
+    //Gyroscope offsets of Mpu6050
+    mpu.gyro_offsets.x = 0.046678342;
+    mpu.gyro_offsets.y = -0.0048800064;
+    mpu.gyro_offsets.z = 0.037488528;
+
+    let mut tracker = Tracker::new();
+    let mut delay = Delay;
+
+    let t = terminal::stdout();
+
+    let mut last = Utc::now();
+    let mut counter = 0;
+    const PC: u32 = 100;
+
+    loop {
+        let acc = mpu.get_acc()?;
+        let gyro = mpu.get_gyro()?;
+
+        let now = Utc::now();
+        let delta = now.signed_duration_since(last);
+        let delta = delta.num_microseconds().unwrap() as f32 / 1_000_000.0;
+        tracker.update(acc, gyro, delta);
+        last = now;
+
+        if counter > PC {
+            t.act(terminal::Action::ClearTerminal(terminal::Clear::All))
+                .unwrap();
+            println!("{:?}\n{:?}", acc, gyro);
+            println!("{:#?}", tracker);
+            println!("delta: {}", delta);
+            counter = 0;
+        }
+        counter += 1;
+        delay.delay_ms(5u16);
+    }
+
+    // Ok(())
+}
---
+++ src/tracker.rs
@@ -0,0 +1,54 @@
+use nalgebra::{base::Vector3, Quaternion, Unit, UnitQuaternion};
+
+#[derive(Debug)]
+pub struct Tracker {
+	pos: Vector3<f32>,
+	vel: Vector3<f32>,
+	rot: UnitQuaternion<f32>,
+}
+
+impl Tracker {
+	pub fn new() -> Self {
+		Tracker {
+			pos: Default::default(),
+			vel: Default::default(),
+			rot: Unit::from_quaternion(Quaternion::from([1.0, 0.0, 0.0, 0.0])),
+		}
+	}
+
+	pub fn update(&mut self, pos_acc: Vector3<f32>, mut rot_vel: Vector3<f32>, delta_time: f32) {
+		//Rotate self.rot by rot_vel
+		rot_vel *= delta_time;
+		let q = UnitQuaternion::from_euler_angles(rot_vel.x, rot_vel.y, rot_vel.z);
+		self.rot *= q;
+
+		//Add the acceleration to the velocity, after rotating it by the local orientation.
+		self.vel += self.rot.inverse_transform_vector(&(pos_acc * delta_time));
+		//The position is automatically corrected for local orientation of the mpu, because the velocity already is.
+		self.pos += self.vel * delta_time;
+	}
+
+	pub fn pos(&self) -> Vector3<f32> {
+		self.pos
+	}
+
+	pub fn vel(&self) -> Vector3<f32> {
+		self.vel
+	}
+
+	pub fn rot(&self) -> UnitQuaternion<f32> {
+		self.rot
+	}
+
+	pub fn pos_mut(&mut self) -> &mut Vector3<f32> {
+		&mut self.pos
+	}
+
+	pub fn vel_mut(&mut self) -> &mut Vector3<f32> {
+		&mut self.vel
+	}
+
+	pub fn rot_mut(&mut self) -> &mut UnitQuaternion<f32> {
+		&mut self.rot
+	}
+}
